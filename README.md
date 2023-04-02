93ae11f6336dbe015429970e801e5ad25207dae2
Natasha <natkamonte1992@gmail.com>
2023-04-02 20:02:01 +0300

    .

---
+++ README.md
@@ -0,0 +1,15 @@
+# Отримуємо і виводимо весь список контактів :
+
+https://monosnap.com/file/poc78WZtQC3rCQFbXMCdDqicWUv1Mg
+
+# Отримуємо контакт по id :
+
+https://monosnap.com/file/hyiG3NS9CvwmgliACqr4EghsRMbTTZ
+
+# Додаємо контакт :
+
+https://monosnap.com/file/EIe3nkKOGjlX7C7NdxpXPLJ6TA7RT0
+
+# Видаляємо контакт :
+
+https://monosnap.com/file/dUiaMLLVVQ5ZfCW5F9ST2xpLQUz0uz
---
+++ contacts.js
@@ -0,0 +1,43 @@
+const { nanoid } = require("nanoid");
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index !== -1) {
+    contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  }
+  return `contact with id ${contactId} was removed successfully`;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf8");
+
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
